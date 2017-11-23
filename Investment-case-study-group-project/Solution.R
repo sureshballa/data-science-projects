3b4bf3fb4a45ba4ca14b92bfca7c703bf5549c10
Suresh Balla <suresh.balla@outlook.com>
2017-11-23 17:48:14 +0530

    Completed checkpoint 3 - country analysis

--- Investment-case-study-group-project/Solution.R
+++ Investment-case-study-group-project/Solution.R
@@ -1,12 +1,13 @@
 library(dplyr)
 
+## Please make sure to set current working directory to same directory where all the files are present
 companies <- read.delim("companies.txt", header = TRUE, sep = "\t", stringsAsFactors = FALSE, encoding = "UTF-8")
 
 ## Remove encoding and have everything to lower so that we can get accurate result to find unique records
 companies$permalink <- iconv(companies$permalink, "UTF-8", "ASCII", sub="")
 companies$permalink <- tolower(companies$permalink)
 
-# write.csv(companies, "companies.csv")
+# Use this command to visualize in excel. write.csv(companies, "companies.csv")
 summary(companies)
 nrow(companies)
 
@@ -36,7 +37,32 @@ nrow(rounds2_multiple_companies)
 round2_new_companies <- subset(rounds2, !(rounds2$company_permalink %in% companies$permalink))
 nrow(round2_new_companies)
 
-master_frame <- merge(companies, rounds2, by.x = "permalink", by.y = "company_permalink")
+master_frame <- merge(companies, rounds2, by.x = "permalink", by.y  = "company_permalink")
 nrow(master_frame)
 
+install.packages("countrycode")
+library("countrycode")
+#countrycode(c('USA', 'DZA', 'BAH'), 'iso3c', 'country.name')
+#custom match is being done for few countries for which countrycode doesnt have mappings
+custom_match <- c(BAH = 'Bahamas', ROM = "Romania", TAN = "Tanzania")
+master_frame$country <- countrycode(master_frame$country_code, "iso3c", "country.name", custom_match = custom_match)
+
+# These below values are from PDF link given
+# Note United Kingdom of Great Britain and Northern Ireland is being considered as english speaking country
+english_speaking_countries_africa <- c('Botswana', 'Cameroon', 'Ethiopia', 'Eritrea', 'The Gambia', 'Ghana', 'Kenya', 'Lesotho', 'Liberia', 'Malawi', 'Mauritius', 'Namibia', 'Nigeria', 'Rwanda', 'Seychelles', 'Sierra Leone', 'South Africa', 'South Sudan', 'Sudan', 'Swaziland', 'Tanzania', 'Uganda', 'Zambia', 'Zimbabwe')
+english_speaking_countries_americas <- c('Antigua and Barbuda', 'The Bahamas', 'Barbados', 'Belize', 'Canada', 'Dominica', 'Grenada', 'Guyana', 'Jamaica', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Trinidad and Tobago', 'United States', 'United States of America', 'United Kingdom of Great Britain and Northern Ireland')
+english_speaking_countries_asia <- c('India', 'Pakistan', 'Philippines', 'Singapore')
+english_speaking_countries_australia <- c('Australia', 'Fiji', 'Kiribati', 'Marshall Islands', 'Federated States of Micronesia', 'Nauru', 'New Zealand', 'Palau', 'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga', 'Tuvalu', 'Vanuatu')
+
+all_english_speaking_countries <- c(english_speaking_countries_africa, english_speaking_countries_americas, english_speaking_countries_asia, english_speaking_countries_australia)
+
+master_frame$is_english_speaking <- sapply(master_frame$country, function(colValue) is.element(colValue, all_english_speaking_countries))
+
 avg_funding <- aggregate(master_frame$raised_amount_usd, by=list(master_frame$funding_round_type), FUN = mean, na.action=na.pass, na.rm=TRUE)
+avg_funding$fund_raised_in_million <- avg_funding$x / 1000000
+
+#above query yeilds funding type of post_ipo_deb has most investments
+investments_for_post_ipo_debt <- filter(master_frame, funding_round_type == "post_ipo_debt", is_english_speaking == TRUE)
+top9_english_speaking_countries_of_investments_for_post_ipo_debt <- group_by(investments_for_post_ipo_debt, country)
+top9 <- summarise(top9_english_speaking_countries_of_investments_for_post_ipo_debt, raised_amount_usd = sum(raised_amount_usd, na.rm = T))
+arrange(top9, desc(top9$raised_amount_usd))
