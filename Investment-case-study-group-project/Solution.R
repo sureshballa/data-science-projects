9c3de62117feb102c60ac96d8d21117767ae027c
Suresh Balla <suresh.balla@outlook.com>
2017-12-01 11:51:19 +0530

    Integrated deepak's other solutions (sql way querying) and update table 5.1 resutls

--- Investment-case-study-group-project/Solution.R
+++ Investment-case-study-group-project/Solution.R
@@ -8,7 +8,7 @@ companies <- read.delim("companies.txt", header = TRUE, sep = "\t", stringsAsFac
 companies$permalink <- iconv(companies$permalink, "UTF-8", "ASCII", sub="")
 companies$permalink <- tolower(companies$permalink)
 
-# Use this command to visualize in excel. write.csv(companies, "companies.csv")
+## Use this command to visualize in excel. write.csv(companies, "companies.csv")
 summary(companies)
 nrow(companies)
 
@@ -23,23 +23,29 @@ rounds2$company_permalink <- tolower(rounds2$company_permalink)
 nrow(rounds2)
 summary(rounds2)
 
-#19990 has NA for raised_amount_usd
+## 19990 has NA for raised_amount_usd
 
+## unique companies 
 distinct_companies <- distinct(companies, permalink)
 distinch_companies_round2 <- distinct(rounds2, company_permalink)
 
+## other approach
+#library("sqldf")
+#sqldf("select Count(distinct company_permalink) as FundedCompanyCount from rounds2;")
+
+## unique companies in rounds2/ Funded 
 nrow(distinct_companies)
 nrow(distinch_companies_round2)
+## other approach
+#sqldf("select Count(distinct permalink) as CompanyCount from companies;")
 
-rounds2_company_groups <- group_by(rounds2, company_permalink)
-rounds2_company_groups_summary <- summarise(rounds2_company_groups, count = n())
-rounds2_multiple_companies <- filter(rounds2_company_groups_summary, count > 1)
-sum(rounds2_multiple_companies$count)
-nrow(rounds2_multiple_companies)
-
+## companies in the rounds2 file which are not present in companies 
 round2_new_companies <- subset(rounds2, !(rounds2$company_permalink %in% companies$permalink))
 nrow(round2_new_companies)
+## other approach
+#setdiff(levels(rounds2$company_permalink),levels(companies$permalink))
 
+## build master frame - merge companies & rounds2 
 master_frame <- merge(companies, rounds2, by.x = "permalink", by.y  = "company_permalink")
 nrow(master_frame)
 
@@ -53,6 +59,11 @@ avg_funding$fund_raised_in_million <- avg_funding$x / 1000000
 ## Spark Funds wants to choose one of these four investment types for each potential investment they will make. Four investments mentioned are Seed, angel, venture, Private equity
 avg_funding_with_constrained_funding_types <- filter(avg_funding, avg_funding$Group.1 == "venture" | avg_funding$Group.1 == "angel" | avg_funding$Group.1 == "seed" | avg_funding$Group.1 == "private_equity")
 arrange(avg_funding_with_constrained_funding_types, desc(avg_funding_with_constrained_funding_types$fund_raised_in_million))
+
+## other approach
+#sqldf("select funding_round_type, Avg(raised_amount_usd) 
+#      from rounds2 where funding_round_type in ('venture', 'angel', 'seed', 'private_equity') 
+#      group by funding_round_type;")
 
 ## Above query yeilds funding type of venture has most investments. So lets go ahead with venture for next steps
 
@@ -78,10 +89,22 @@ all_english_speaking_countries <- c(english_speaking_countries_africa, english_s
 
 master_frame$is_english_speaking <- sapply(master_frame$country, function(colValue) is.element(colValue, all_english_speaking_countries))
 
-investments_for_post_ipo_debt <- filter(master_frame, funding_round_type == "venture", is_english_speaking == TRUE)
-top9_english_speaking_countries_of_investments_for_post_ipo_debt <- group_by(investments_for_post_ipo_debt, country)
-top9 <- summarise(top9_english_speaking_countries_of_investments_for_post_ipo_debt, raised_amount_usd = sum(raised_amount_usd, na.rm = T))
-arrange(top9, desc(top9$raised_amount_usd))
+## top 9
+investments_venture <- filter(master_frame, funding_round_type == "venture")
+top_countries_of_investments_group <- group_by(investments_venture, country)
+top_countries_of_investments <- summarise(top_countries_of_investments_group, raised_amount_usd = sum(raised_amount_usd, na.rm = T))
+top9 = head(arrange(top_countries_of_investments, desc(top_countries_of_investments$raised_amount_usd)), 9)
+
+## top english speaking countries by investment within the top 9 set
+top9$country[which(is.element(top9$country, all_english_speaking_countries))]
+
+## other approach
+#top9 <- sqldf("select country_code, sum(raised_amount_usd)
+#           from master_frame
+#           where funding_round_type='venture'
+#           group by country_code
+#           order by 2 desc
+#           LIMIT 9;")
 
 ## Above query yeilds top 3 countries as United States of America, United Kingdom of Great Britain and Northern Ireland, India
 ## Assumption here is - "United Kingdom of Great Britain and Northern Ireland" is english speaking country - meaning english as official language
@@ -112,7 +135,7 @@ getMainSector <- function(primary_sector_value) {
   matchRecord$sector[1]
 }
 
-#sapply is taking time, wait for statement to complete execution
+#sapply is taking time, wait for statement to complete execution. TODO: Need to optimise this, will work on it
 master_frame$main_sector <- sapply(master_frame$primary_sector, getMainSector)
 
 ## End of Checkpoint 4: Sector Analysis 1
@@ -135,6 +158,14 @@ sector_groups_d3_summary <- summarise(sector_groups_d3, count = n(), total_amoun
 D3 <- arrange(sector_groups_d3_summary, desc(total_amount_invested))
 
 ## End of Checkpoint 5: Sector Analysis 2
+
+## Answers for Table 5.1 : Sector-wise Investment Analysis
+## Requirement: all the observations refer to investments of the type FT within 5-15 M USD range. 
+## So we will fiter FT = venture and not really by range
+## Will save D1, D2 and D3 as csv and find answers for Table 5.1 questions
+## write.csv(D1, "D1.csv")
+## write.csv(D2, "D2.csv")
+## write.csv(D3, "D3.csv")
 
 ## Use below command to export all computed data and use it in tableau for plots
 ## write.csv(master_frame, "master_frame.csv")
